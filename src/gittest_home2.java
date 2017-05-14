918c2cf992cf77ce5b8b7d433c6daa0c4af11ead
liangtl2 <liangtl2@lenovo.com>
2017-05-14 19:42:08 +0800

    Something not right 2

--- src/gittest_home2.java
+++ src/gittest_home2.java
@@ -1,8 +1,8 @@
 
 public class gittest_home2 {
 	public static void main(String args[]){
+		System.out.println("something not right 2");
 		System.out.println("1st IDE commit");
-		System.out.println("something not right");
 	}
 
 }
