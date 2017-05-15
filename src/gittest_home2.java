307aee621baca01f45947b3b79a328ee01f7105c
liangtl2 <liangtl2@lenovo.com>
2017-05-15 18:20:53 +0800

    try 1st ide again

--- src/gittest_home2.java
+++ src/gittest_home2.java
@@ -1,9 +1,7 @@
 
 public class gittest_home2 {
 	public static void main(String args[]){
-		System.out.println("something not right 2");
 		System.out.println("1st IDE commit");
-		System.out.println("try again");
 	}
 
 }
