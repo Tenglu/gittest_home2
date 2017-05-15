f5577d7dbc47f2774f53e94e9822f6bce77fbbf3
liangtl2 <liangtl2@lenovo.com>
2017-05-15 18:55:03 +0800

    merge conflict

--- src/gittest_home2.java
+++ src/gittest_home2.java
@@ -3,6 +3,7 @@ public class gittest_home2 {
 	public static void main(String args[]){
 		System.out.println("2nd IDE commit");
 		System.out.println("1st IDE commit");
+		System.out.println("1st IDE commit again");
 	}
 
 }
