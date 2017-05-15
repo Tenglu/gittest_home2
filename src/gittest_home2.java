30f51904285d80074aa2ad238b36a8e9afed65ba
liangtl2 <liangtl2@lenovo.com>
2017-05-15 19:15:15 +0800

    push

--- src/gittest_home2.java
+++ src/gittest_home2.java
@@ -1,8 +1,8 @@
 
 public class gittest_home2 {
 	public static void main(String args[]){
-		System.out.println("2nd IDE commit");
-
+		System.out.println("1st IDE commit");
+		System.out.println("1st IDE commit again");
 	}
 
 }
